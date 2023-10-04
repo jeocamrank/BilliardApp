5ff94abb849257707eb79a8b3ced8805a0df1b7a
jeocamrank <104308268+jeocamrank@users.noreply.github.com>
2023-10-04 20:59:45 +0700

    First push

---
+++ BilliardApp/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace BilliardApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSlide = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnTable = new System.Windows.Forms.Button();
+            this.panelSubProduct = new System.Windows.Forms.Panel();
+            this.btnBrand = new System.Windows.Forms.Button();
+            this.btnCategory = new System.Windows.Forms.Button();
+            this.btnProductList = new System.Windows.Forms.Button();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelSlide.SuspendLayout();
+            this.panelSubProduct.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSlide
+            // 
+            this.panelSlide.Controls.Add(this.btnLogout);
+            this.panelSlide.Controls.Add(this.btnTable);
+            this.panelSlide.Controls.Add(this.panelSubProduct);
+            this.panelSlide.Controls.Add(this.btnProduct);
+            this.panelSlide.Controls.Add(this.btnDashboard);
+            this.panelSlide.Controls.Add(this.panelLogo);
+            this.panelSlide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSlide.Location = new System.Drawing.Point(0, 0);
+            this.panelSlide.Name = "panelSlide";
+            this.panelSlide.Size = new System.Drawing.Size(200, 653);
+            this.panelSlide.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(0, 608);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnLogout.Size = new System.Drawing.Size(200, 45);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // btnTable
+            // 
+            this.btnTable.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTable.FlatAppearance.BorderSize = 0;
+            this.btnTable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTable.ForeColor = System.Drawing.Color.White;
+            this.btnTable.Location = new System.Drawing.Point(0, 395);
+            this.btnTable.Name = "btnTable";
+            this.btnTable.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnTable.Size = new System.Drawing.Size(200, 45);
+            this.btnTable.TabIndex = 3;
+            this.btnTable.Text = "Table";
+            this.btnTable.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTable.UseVisualStyleBackColor = true;
+            this.btnTable.Click += new System.EventHandler(this.btnTable_Click);
+            // 
+            // panelSubProduct
+            // 
+            this.panelSubProduct.Controls.Add(this.btnBrand);
+            this.panelSubProduct.Controls.Add(this.btnCategory);
+            this.panelSubProduct.Controls.Add(this.btnProductList);
+            this.panelSubProduct.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSubProduct.Location = new System.Drawing.Point(0, 260);
+            this.panelSubProduct.Name = "panelSubProduct";
+            this.panelSubProduct.Size = new System.Drawing.Size(200, 135);
+            this.panelSubProduct.TabIndex = 2;
+            // 
+            // btnBrand
+            // 
+            this.btnBrand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(100)))));
+            this.btnBrand.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBrand.FlatAppearance.BorderSize = 0;
+            this.btnBrand.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrand.ForeColor = System.Drawing.Color.White;
+            this.btnBrand.Location = new System.Drawing.Point(0, 90);
+            this.btnBrand.Name = "btnBrand";
+            this.btnBrand.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnBrand.Size = new System.Drawing.Size(200, 45);
+            this.btnBrand.TabIndex = 4;
+            this.btnBrand.Text = "Brand";
+            this.btnBrand.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBrand.UseVisualStyleBackColor = false;
+            this.btnBrand.Click += new System.EventHandler(this.btnBrand_Click);
+            // 
+            // btnCategory
+            // 
+            this.btnCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(100)))));
+            this.btnCategory.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCategory.FlatAppearance.BorderSize = 0;
+            this.btnCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategory.ForeColor = System.Drawing.Color.White;
+            this.btnCategory.Location = new System.Drawing.Point(0, 45);
+            this.btnCategory.Name = "btnCategory";
+            this.btnCategory.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnCategory.Size = new System.Drawing.Size(200, 45);
+            this.btnCategory.TabIndex = 3;
+            this.btnCategory.Text = "Category";
+            this.btnCategory.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCategory.UseVisualStyleBackColor = false;
+            this.btnCategory.Click += new System.EventHandler(this.btnCategory_Click);
+            // 
+            // btnProductList
+            // 
+            this.btnProductList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(100)))));
+            this.btnProductList.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProductList.FlatAppearance.BorderSize = 0;
+            this.btnProductList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductList.ForeColor = System.Drawing.Color.White;
+            this.btnProductList.Location = new System.Drawing.Point(0, 0);
+            this.btnProductList.Name = "btnProductList";
+            this.btnProductList.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.btnProductList.Size = new System.Drawing.Size(200, 45);
+            this.btnProductList.TabIndex = 2;
+            this.btnProductList.Text = "Product List";
+            this.btnProductList.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductList.UseVisualStyleBackColor = false;
+            this.btnProductList.Click += new System.EventHandler(this.btnProductList_Click);
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProduct.FlatAppearance.BorderSize = 0;
+            this.btnProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProduct.ForeColor = System.Drawing.Color.White;
+            this.btnProduct.Location = new System.Drawing.Point(0, 215);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnProduct.Size = new System.Drawing.Size(200, 45);
+            this.btnProduct.TabIndex = 1;
+            this.btnProduct.Text = "Product";
+            this.btnProduct.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.ForeColor = System.Drawing.Color.White;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 170);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnDashboard.Size = new System.Drawing.Size(200, 45);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Controls.Add(this.lblUserName);
+            this.panelLogo.Controls.Add(this.lblRole);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 170);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.ForeColor = System.Drawing.Color.White;
+            this.lblUserName.Location = new System.Drawing.Point(49, 113);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(100, 21);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "UserName";
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.ForeColor = System.Drawing.Color.White;
+            this.lblRole.Location = new System.Drawing.Point(37, 144);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(126, 21);
+            this.lblRole.TabIndex = 1;
+            this.lblRole.Text = "Administrator";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BilliardApp.Properties.Resources.administrator_male_80px1;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.Controls.Add(this.lblTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(200, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(982, 40);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(294, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(384, 21);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "TitleName";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.Color.White;
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(200, 40);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(982, 613);
+            this.panelMain.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelSlide);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelSlide.ResumeLayout(false);
+            this.panelSubProduct.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelTitle.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSlide;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Panel panelSubProduct;
+        private System.Windows.Forms.Button btnBrand;
+        private System.Windows.Forms.Button btnCategory;
+        private System.Windows.Forms.Button btnProductList;
+        private System.Windows.Forms.Button btnTable;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
+
---
+++ BilliardApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BilliardApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cm = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        public Form1()
+        {
+            InitializeComponent();
+            customizeDesing();
+            cn = new SqlConnection(dbcon.myConnection());
+            cn.Open();
+        }
+        #region panelSlide
+        public void customizeDesing()
+        {
+            panelSubProduct.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if(panelSubProduct.Visible == true)
+            {
+                panelSubProduct.Visible = false;
+            }
+        }
+
+        private void showSubMenu(Panel submenu)
+        {
+            if(submenu.Visible == false)
+            {
+                hideSubMenu();
+                submenu.Visible = true;
+            }
+            else
+            {
+                submenu.Visible= false;
+            }
+        }
+        #endregion panelSlide
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProduct_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelSubProduct);
+        }
+        private void btnProductList_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void btnCategory_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void btnBrand_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void btnTable_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
